8acd5dd859880f46fd3736d6559c2f9b8fb204f2
SnowWhite1129 <gary8511@gmail.com>
2020-03-21 15:09:21 +0800

    Init

--- netstat.cpp
+++ netstat.cpp
@@ -10,6 +10,61 @@
 #include <netdb.h>
 #include <errno.h>
 #include <dirent.h>
+#include <vector>
+#include <string>
+#include <ctype.h>
+
+#define MAX_LINE 1024
+using namespace std;
+struct Address{
+    char IP[16];
+    uint32_t port;
+};
+struct Connection{
+    string protocol;
+    Address local, remote;
+    int pid;
+    unsigned int inode;
+    char program[MAX_LINE];
+    void Print();
+};
+struct Process{
+    int pid;
+    char program[MAX_LINE];
+    vector <unsigned int > inode;
+    void ReadProgram(string filename);
+    void Readinode
+};
+void Connection::Print() {
+    printf("%-6s %-8s:%-4u %-8s:%-4u %d/%s\n", protocol.c_str(), local.IP, local.port, remote.IP, remote.port, pid, program);
+}
+void ReadAddress(const char filename[]){
+    FILE *input;
+    input = fopen(filename, "r");
+
+    char line[MAX_LINE];
+
+    while (!feof(input)){
+        fgets(line, MAX_LINE, input);
+
+    }
+
+    fclose(input);
+}
+void Process::ReadProgram(string filename){
+    filename += "cmd_line";
+
+    FILE *input;
+    input = fopen(filename.c_str(), "r");
+
+    fgets(program, MAX_LINE, input);
+
+    fclose(input);
+}
+
+vector <Connection> connections;
+vector <Process> processes;
+
 int main()
 {
 	struct in_addr ip_addr;
@@ -17,14 +72,23 @@ int main()
 	uint32_t port = 0x18EB;
 	printf("IP Address = %s %u\n", inet_ntoa(ip_addr), port);
 
-	DIR *dirp = opendir("/proc/net");
+	DIR *dirp = opendir("/proc");
 	if (dirp){
-		printf("Entry\n");
 		struct dirent *direntp;
 		while( (direntp = readdir(dirp)) != NULL ){
-			printf("%s\n", direntp->d_name);
+		    char name[MAX_LINE];
+		    strcpy(name, direntp->d_name);
+		    if (isdigit(name[0])){
+		        printf("%s\n", direntp->d_name);
+
+                string dir = "/proc/" + name + "/";
+		        Process process;
+		        process.ReadProgram(dir);
+
+		        string fdname = dir + "fd/";
+		    }
 		}
 		closedir(dirp);
 	}
-	exit(0);
+
 }
